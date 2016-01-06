789f48d76b68b67f3c8f735e417306fe5ce6bc1b
ArmanT4t1qbit <arman.tavakoli@1qbit.com>
2016-01-06 15:44:37 -0800

    Update SomeName2.py

--- SomeName2.py
+++ SomeName2.py
@@ -1,3 +1,4 @@
 print "This is going to be a good day, now edited, edit 2."
 print "second line, want to go from left to right in the merge tool with this line."
 print "some other statement" 
+print "fourth"
